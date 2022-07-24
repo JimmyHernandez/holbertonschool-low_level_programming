716c58d6c545a69d60ac8b2d14fc3fd458983420
JimmyHernandez <jimmy.hernandez-rivera@hobeltonschool.com>
2022-07-24 15:16:25 +0000

    first commit

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int lq;
+
+	for (lq = '0'; lq <= '9'; lq++)
+	{
+	putchar(lq);
+	}
+	putchar('\n');
+	return (0);
+}
