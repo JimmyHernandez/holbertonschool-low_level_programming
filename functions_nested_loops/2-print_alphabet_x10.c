010d047699ddd10ca6fa325532069e711c8009ad
JimmyHernandez <jimoem24@gmail.com>
2022-07-27 11:21:57 -0400

    first commit

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - print alphabet 10 time
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int n=0;
+		
+	for(n=0; n <= 9; n++)
+	{	
+		for (i = 'a'; i <= 'z'; i++)
+			{
+			_putchar(i);
+			
+			} 
+			_putchar('\n');
+	} 
+	n++;
+
+
+}
