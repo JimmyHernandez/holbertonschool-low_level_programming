2328d67d948524d23f0e75be9a981e6de57a2047
JimmyHernandez <jimoem24@gmail.com>
2022-07-27 16:58:06 -0400

    first commit

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_abs - absolute value of integer
+*@n: Input value
+*Return: 0.
+*
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+	return (n * -1);
+	}
+	else
+	{
+	return (n);
+	}
+}
