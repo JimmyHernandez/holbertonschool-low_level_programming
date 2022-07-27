f87616158afadb4a900e987451ca610b72ad72b3
JimmyHernandez <jimoem24@gmail.com>
2022-07-27 15:28:13 -0400

    first commit

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _isalpha: checks for alphabetic character
+*@c: input
+*Return: 1 alpha 0 success.
+*/
+int _isalpha(int c)
+{
+	if (((c >= 'A') && (c <= 'Z')) || ((c >= 'a') && (c <= 'z')))
+		return (1);
+	else
+		return (0);
+}
