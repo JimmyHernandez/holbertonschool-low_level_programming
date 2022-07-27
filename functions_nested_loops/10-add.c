f551bd8a57e1749e8821619df53c114d2578d13e
JimmyHernandez <jimoem24@gmail.com>
2022-07-27 17:46:40 -0400

    first commit

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* add - sum two integers
+*@first: first number
+*@second: second number
+*Return: the add from first and second.
+*/
+int add(int first, int second)
+{
+return ((first + second));
+}
