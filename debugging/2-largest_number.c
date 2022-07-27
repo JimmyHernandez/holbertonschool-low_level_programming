7d6902c1c007a0c886f660226a30ddec2dce131d
JimmyHernandez <jimoem24@gmail.com>
2022-07-27 19:11:13 -0400

    first commit

--- debugging/2-largest_number.c
+++ debugging/2-largest_number.c
@@ -10,20 +10,21 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b && b > c)
-    {
-        largest = b;
-    }
-    else if (b > a && a > c)
-    {
-        largest = c;
-    }
-    else
-    {
-        largest = a;
-    }
+	if (a > b && b > c)
+	{
+	largest = c;
+	}
+
+	else if (b > a && a > c)
+	{
+	largest = b;
+	}
+	else
+	{
+	largest = a;
+	}
 
     return (largest);
 }
