830183defc31f73413bfc5de3695422565580147
JimmyHernandez <jimmy.hernandez-rivera@hobeltonschool.com>
2022-08-01 14:26:12 +0000

    first commit

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase letter.
+ * @c : input data
+ * Return: 0 if lower case, 1 uppercase.
+ */
+
+int _isupper(int c)
+{
+	if (c < 'A' || c > 'Z')
+	{
+	return (0);
+	}
+	else
+	return (1);
+
+}
