2f2f57e34dc73e8c2af02016543dd47b13f54f3b
JimmyHernandez <jimmy.hernandez-rivera@hobeltonschool.com>
2022-08-01 19:29:31 +0000

    first commit

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiply a * b
+ *@a: first number
+ *@b: second numer
+ *Return: mult;
+ */
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+	return (mult);
+}
