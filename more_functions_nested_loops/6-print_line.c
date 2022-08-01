aff175ec913f094977176338ced71e3e8e79b35c
JimmyHernandez <jimmy.hernandez-rivera@hobeltonschool.com>
2022-08-01 20:18:24 +0000

    first commit

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *
+ * print_line - print lines
+ *
+ *@n: input
+ *
+ */
+void print_line(int n)
+{
+
+	int i;
+
+		if (n > 0)
+
+			_putchar('\n');
+		
+		for (i = 0 ; i <= n; i++)
+
+		{
+			_putchar('_');
+		}
+	_putchar('\n');
+}
