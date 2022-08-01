49a663c13eff4d6f32feec1a85c2a78487a2824a
JimmyHernandez <jimmy.hernandez-rivera@hobeltonschool.com>
2022-08-01 14:03:45 +0000

    first commit

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-
 #ifndef MAIN_H_
 #define MAIN_H_
 
@@ -6,5 +5,16 @@
 #include <string.h>
 
 int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
 
 #endif
