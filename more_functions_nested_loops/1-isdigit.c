097c1a326a44f4731979262ee9cc8b6b1d52d4d8
JimmyHernandez <jimmy.hernandez-rivera@hobeltonschool.com>
2022-08-01 19:22:49 +0000

    first commit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for number 0-9.
+ * @c : input data
+ * Return: 0 if a letter, 1 is digit.
+ */
+
+int _isdigit(int c)
+{
+	if (c < '0' || c > '9')
+	{
+	return (0);
+	}
+	else
+	return (1);
+
+}
+
