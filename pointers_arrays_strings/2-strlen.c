1f1e5a46a8ddb463b42ce01e6702ce0a0cbd4ac6
JimmyHernandez <jimoem24@gmail.com>
2022-08-09 22:26:01 -0400

    first commit

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - size in character of input
+ *@s: input tada
+ *Return: i
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
