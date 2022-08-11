34805b2fa0a42087b8ea7d5a2af7f0fef646253e
JimmyHernandez <jimoem24@gmail.com>
2022-08-11 19:41:23 +0000

    first commit

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - check the code
+ * @str : pointer
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int ln = 0, i, num;
+
+	while (str[ln] != '\0')
+		ln++;
+
+	if (ln % 2 == 0)
+		num = ln / 2;
+
+	else
+		num = (ln + 1) / 2;
+
+	for (i = num; i < ln; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
