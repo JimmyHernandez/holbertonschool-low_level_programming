ba797c071f8314605bfa57fb907c7130e0e73837
JimmyHernandez <jimoem24@gmail.com>
2022-08-25 10:43:23 -0400

    first commit

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - prints chessboard
+ * @a: two dimensional array for chessboard
+ **/
+void print_chessboard(char (*a)[8])
+{
+	int n, b;
+
+	for (n = 0; n < 8; n++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[n][b]);
+		}
+		_putchar('\n');
+	}
+}
