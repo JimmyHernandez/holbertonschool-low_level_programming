1640e918c6b57f69f1c19aba9ed3782d061c5f03
JimmyHernandez <jimoem24@gmail.com>
2022-08-31 14:27:11 -0400

    second commit

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -5,11 +5,9 @@
  *@argv: vector for strings
  *Return: 0
  */
-int main(int argc, char *argv[] __attribute__((unused)) )
+int main(int argc, char *argv[] __attribute__((unused)))
 {
- 		
 	printf("%d\n", argc - 1);
-	
-	
-        return (0);
+
+return (0);
 }
