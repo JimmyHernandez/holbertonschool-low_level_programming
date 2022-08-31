35116044b197a4de09fb2c023b21c513fd94f97b
JimmyHernandez <jimoem24@gmail.com>
2022-08-31 15:15:56 -0400

    first commit

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *@argc: counts arguments
+ *@argv: vector for strings
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{ 
+	if (argc > 2)
+
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+
+	else
+
+	{
+		printf("Error\n");
+
+	}
+
+	return (0);
+}
