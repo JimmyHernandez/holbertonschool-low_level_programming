05679efa9d5d8af43d81190f75db716bdb40b945
JimmyHernandez <jimoem24@gmail.com>
2022-08-31 13:38:25 -0400

    first commit

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *@argc: counts arguments
+ *@argv: vector for strings
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int idx;
+
+	for (idx = 0; idx < argc; idx++)
+	{
+		printf("%s\n", argv[idx]);
+	}
+
+	return (0);
+}
