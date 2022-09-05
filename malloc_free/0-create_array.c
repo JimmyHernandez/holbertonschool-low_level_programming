9a2bffbf9ae01647a23a0782b99ba258663816cf
JimmyHernandez <jimoem24@gmail.com>
2022-09-05 18:12:53 -0400

    fist commit

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,4 +1,4 @@
-#inclide "main.h"
+#include "main.h"
 /**
  * create_array - crear un arreglo
  *@size: size
@@ -7,6 +7,25 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *arr;
+	unsigned int idx;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
 
+	arr = malloc(sizeof(c) * size);
+
+	if (arr == NULL)
+
+	{
+	return (NULL);
+	}
+
+	for (idx = 0; idx < size; idx++)
+	{
+		arr[idx] = c;
+	}
+	return (arr);
 }
