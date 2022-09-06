c126b10620d096d8678751eec89d47520620c615
JimmyHernandez <jimoem24@gmail.com>
2022-09-06 12:09:48 -0400

    4th commit

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,14 +11,15 @@ char *str_concat(char *s1, char *s2)
 	char *content;
 	unsigned int sum;
 
-	if (s1 == NULL)
-	{
-		s1 = " ";
-	}
 	if (s2 == NULL)
 	{
 		s2 = " ";
 	}
+	if (s1 == NULL)
+	{
+		s1 = " ";
+	}
+
 	for (idx1 = 0; s1[idx1] != '\0'; idx1++)
 		{}
 	for (idx2 = 0; s2[idx2] != '\0'; idx2++)
