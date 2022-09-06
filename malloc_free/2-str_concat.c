b545bce75d942119603d3eb04115090a0eceaa00
JimmyHernandez <jimoem24@gmail.com>
2022-09-06 12:34:55 -0400

    sexto

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,21 +13,23 @@ char *str_concat(char *s1, char *s2)
 
 	if (s2 == NULL)
 	{
-		s2 = " ";
+		s2 = "";
 	}
+
 	if (s1 == NULL)
 	{
-		s1 = " ";
+		s1 = "";
 	}
 
-	for (idx1 = 0; s1[idx1] != '\0'; idx1++)
-		{	;	}
-	for (idx2 = 0; s2[idx2] != '\0'; idx2++)
+	for (idx1 = 0; s1[idx1] != '\0'; idx1++)/*string length 1 */
 		{	;	}
 
-	sum = idx1 + idx2;
+	for (idx2 = 0; s2[idx2] != '\0'; idx2++)/*string length 2*/
+		{	;	}
 
-	content = malloc(sizeof(char) * (sum + 1));
+	sum = (idx1 + idx2) + 1;/* sum of strings + 1 space for character '\0'*/
+
+	content = malloc(sizeof(char) * sum);
 
 		if (content == NULL)
 		{
