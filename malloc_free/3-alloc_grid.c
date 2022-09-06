8de6655d0eaaff62edc3f0631f3c9dff7ee7e111
JimmyHernandez <jimoem24@gmail.com>
2022-09-06 18:07:12 -0400

    upgrade

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -7,33 +7,27 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **grid;
-	int i, n;
+	int **arr;
+	int i, j;
 
-	if (width <= 0 || height <= 0)
-		return (NULL);
-
-	grid = malloc(sizeof(int) * height);
-
-	if (grid == NULL)
-	{
-		free(grid);
-		return (NULL);
-	}
-
+	if (width < 1 || height < 1)
+	return (NULL);
+	arr = malloc(sizeof(int *) * height);
+	if (arr == NULL)
+	return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc(sizeof(int) * width);
-		if (grid == NULL)
+		arr[i] = malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
 		{
-			free(grid);
+			for (--i; i >= 0; i--)
+				free(arr[i]);
+			free(arr);
 			return (NULL);
 		}
-
-		for (n = 0; n < width; n++)
-		{
-			grid[i][n] = 0;
-		}
 	}
-	return (grid);
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	return (arr);
 }
