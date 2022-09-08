cbb955fdf7cd659bf7ec828820a7cd814b61f9dc
JimmyHernandez <jimoem24@gmail.com>
2022-09-08 15:24:15 -0400

    first commit

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * array_range - nested loop to make grid
+ * @min: minimun
+ * @max: maximun
+ * Return: pointer
+ */
+int *array_range(int min, int max)
+{
+	int *rng, size, i;
+
+	if (min > max)
+		return (0);
+
+	size = (max + min) + 1;
+
+	rng = malloc(size * sizeof(int));
+
+	if (rng == 0)
+		return (0);
+
+	for (i = 0; i < size; i++)
+	{
+		rng[i] = min + i;
+
+	}
+	return (rng);
+}
