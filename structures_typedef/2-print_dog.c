1d69742a2db2bfd526e42175ae0e3ca4d06b3665
JimmyHernandez <jimoem24@gmail.com>
2022-09-15 15:57:55 -0400

    first commit

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - Prints struct dog.
+ * @d: Name of the struct data type.
+ * Return: 0 (Success).
+ **/
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d == NULL)
+		{
+			return;
+		}
+
+		if (d->name == NULL)
+		{
+			d->name = "(nil)";
+		}
+
+		if (d->owner == NULL)
+		{
+			d->owner = "(nil)";
+		}
+		printf("Name: %s\n", d->name);
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+}
