8575e632c7bb9f82e8e78c895d5a167aa455632f
JimmyHernandez <you@example.com>
2022-10-23 21:39:39 +0000

    first commit

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ *free_listint2 - Frees a list and sets the head to NULL
+ *@head: Head of the list
+ **/
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp, *current;
+
+	if (head != NULL)
+	{
+		current = *head;
+		while (current != NULL)
+		{
+			tmp = current;
+			current = current->next;
+			free(tmp);
+		}
+	}
+	*head = NULL;
+}
+
