c137c802ae400a50b0b39cc9802b6fd3a764ec92
JimmyHernandez <you@example.com>
2022-10-23 18:30:02 +0000

    trailing space at line 16

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -12,8 +12,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *newNode = malloc(sizeof(listint_t));
 
 	if (newNode == NULL)
-	{ return (NULL); }
-	
+	{
+		return (NULL);
+	}
+
 	newNode->n = n;
 	newNode->next = *head;
 	*head = newNode;
