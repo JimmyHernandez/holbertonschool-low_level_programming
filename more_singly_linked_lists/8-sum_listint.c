ce294ea5e6b5301e34fb6186e26d8b6cf66ae698
JimmyHernandez <you@example.com>
2022-10-25 22:09:26 +0000

    space before open parentesis

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -10,7 +10,7 @@ int sum_listint(listint_t *head)
 
 	if (head == NULL)
 	{
-		return(0);
+		return (0);
 	}
 
 	while (head != NULL)
@@ -18,7 +18,7 @@ int sum_listint(listint_t *head)
 		sum += head->n;
 		head = head->next;
 	}
-	return(sum);
+	return (sum);
 
 
 }
