12b7229691b2d25879dff67ec6c3542aaa9c860e
JimmyHernandez <you@example.com>
2022-10-27 14:13:45 +0000

    first commit

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *print_binary - convert from decimal to binary
+ *@n: number to convert
+ */
+void print_binary(unsigned long int n)
+{
+	int idx, Counter = 0;
+	unsigned long int Current;
+
+	for (idx = 63; idx >= 0; idx--)
+	{
+		Current = n >> idx;
+		
+		if (Current & 1)
+		{
+			_putchar('1');
+			Counter++;
+		}
+		else if (Counter)
+		{
+			_putchar('0');
+		}
+	}
+
+	if (!Counter)
+	{
+		_putchar('0');
+	}
+}
