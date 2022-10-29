7ac74d9485e6c5d84cc05d3f23827fe4d02f90f0
Jimmy Hernandez Rivera <105442329+JimmyHernandez@users.noreply.github.com>
2022-10-29 09:56:21 -0400

    Update 5-free_listint2.c

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -5,20 +5,13 @@
  **/
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp, *current;
-
-	current = *head;
+	listint_t *tmp;
 	
-	if (*head == NULL)
+	while (head && *head)
 	{
-	return;
-	}
-
-	while (head != NULL)
-	{
-	tmp = current;
-	current = current->next;
-	free(tmp);
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
 	}
 
 }
