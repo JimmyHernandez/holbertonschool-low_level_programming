74635fd3b9eab32475781dc9c7e551a33c79d72e
Jimmy Hernandez Rivera <105442329+JimmyHernandez@users.noreply.github.com>
2022-10-30 19:44:31 -0400

    Update 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -8,44 +8,35 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *NNode;
-	dlistint_t *Head;
-	unsigned int INode;
+	dlistint_t *tmp = *h, *tmpcount = *h, *tmp2;
+	dlistint_t *new_node;
+	unsigned int cont = 0;
 
-	NNode = NULL;
+	if (*h == NULL)
+		return (NULL);
+	while (tmpcount != NULL)
+		tmpcount = tmpcount->next, cont++;
+	if (idx > cont)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
 	if (idx == 0)
-		NNode = add_dnodeint(h, n);
-	else
 	{
-		Head = *h;
-		INode = 1;
-		if (Head != NULL)
-			while (Head->prev != NULL)
-				Head = Head->prev;
-		while (Head != NULL)
-		{
-			if (INode == idx)
-			{
-				if (Head->next == NULL)
-					NNode = add_dnodeint_end(h, n);
-				else
-				{
-					NNode = malloc(sizeof(dlistint_t));
-					if (NNode != NULL)
-					{
-						NNode->n = n;
-						NNode->next = Head->next;
-						NNode->prev = Head;
-						Head->next->prev = NNode;
-						Head->next = NNode;
-					}
-				}
-				break;
-			}
-		}
-		Head = Head->next;
-		INode++;
+		new_node->prev = NULL, new_node->next = *h;
+		if (*h)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
 	}
-	return (NNode);	
+	for (cont = 0; cont < idx; cont++)
+		tmp2 = tmp, tmp = tmp->next;
+	new_node->next = tmp;
+	new_node->prev = tmp2;
+	tmp2->next = new_node;
+	if (tmp)
+		tmp->prev = new_node;
+	return (new_node);
 
 }
