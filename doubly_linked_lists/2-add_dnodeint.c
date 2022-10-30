92440b7611ce63d597608a8cf3c025ce9004558a
JimmyHernandez <you@example.com>
2022-10-30 23:04:49 +0000

    first commit

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ *add_dnodeint - add an integer to the head
+ *@head: the head of the list
+ *@n: number to be add
+ *Return: newNode or NULL if fails
+ *
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+
+	listint_t *newNode = malloc(sizeof(listint_t));
+
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+
+	newNode->n = n;
+	newNode->next = *head;
+	*head = newNode;
+
+	return (newNode);
+}
+
