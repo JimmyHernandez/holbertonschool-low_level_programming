d328cb1da82ba4d19b5aa29e8010d5042d53eb6b
JimmyHernandez <you@example.com>
2022-10-30 22:57:57 +0000

    first commit

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *print_listint - print list of numbers
+ *@h: head
+ *Return: idx.
+ */
+size_t print_dlistint(const listint_t *h)
+{
+	int idx = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		idx++;
+	}
+	return (idx);
