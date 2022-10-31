fc81be8e3fa463a59f862ff223124cc13eeb0d9e
Jimmy Hernandez Rivera <105442329+JimmyHernandez@users.noreply.github.com>
2022-10-31 10:40:22 -0400

    Update 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -7,36 +7,45 @@
  *Return: new node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
-{
-	dlistint_t *tmp = *h, *tmpcount = *h, *tmp2;
-	dlistint_t *new_node;
-	unsigned int cont = 0;
+{	
+dlistint_t *current, *new;
+	unsigned int counter = 0;
 
-	if (*h == NULL)
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
-	while (tmpcount != NULL)
-		tmpcount = tmpcount->next, cont++;
-	if (idx > cont)
-		return (NULL);
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
+
+	current = *h;
+	new->prev = NULL;
+	new->next = NULL;
+	new->n = n;
 	if (idx == 0)
 	{
-		new_node->prev = NULL, new_node->next = *h;
-		if (*h)
-			(*h)->prev = new_node;
-		*h = new_node;
-		return (new_node);
+		if (*h == NULL)
+			*h = new;
+		else
+		{
+			new->next = *h;
+			current->prev = new;
+			*h = new;
+		}
+		return (new);
 	}
-	for (cont = 0; cont < idx; cont++)
-		tmp2 = tmp, tmp = tmp->next;
-	new_node->next = tmp;
-	new_node->prev = tmp2;
-	tmp2->next = new_node;
-	if (tmp)
-		tmp->prev = new_node;
-	return (new_node);
 
+	while (current->next != NULL || counter + 1 == idx)
+	{
+		if (counter + 1 == idx)
+		{
+			if (current->next != NULL)
+				current->next->prev = new;
+			new->next = current->next;
+			current->next = new;
+			new->prev = current;
+			return (new);
+		}
+		current = current->next;
+		counter++;
+	}
+	free_dlistint(new);
+	return (NULL);
 }
