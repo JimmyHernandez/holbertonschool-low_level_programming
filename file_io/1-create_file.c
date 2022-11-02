7d4e178aa1cf706d4964f402e6f7bce4bcbc473d
JimmyHernandez <you@example.com>
2022-11-02 00:20:47 +0000

    first commit

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,14 +13,25 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	rewrt = write(fd, text_content, buffer);
 
-	if (!filename && fd == -1 && rewrt == -1)
+	
+	if (filename == NULL)
 	{
 		return (-1);
 	}
-	if (!text_content)
+	
+	if (fd == -1 && rewrt == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
 	{
 		text_content = "";
 	}
+
+	if (fd == 0000)
+	{
+		return (-1);
 
 	close(fd);
 	return (1);
