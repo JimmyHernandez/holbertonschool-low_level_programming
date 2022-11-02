c5a9bbac2809ca6271786ea2a2ce2366525301d8
JimmyHernandez <you@example.com>
2022-11-02 00:46:39 +0000

    fix edge case

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -10,7 +10,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, rewtr, buffer = strlen(text_content);
 
-	fd = open(filename, O_WRONLY | O_APPEND | O_TRUNC);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
 	{
