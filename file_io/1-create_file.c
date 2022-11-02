b5d706a370047ec40220c85de9d4cc5ca012c412
Jimmy Hernandez Rivera <105442329+JimmyHernandez@users.noreply.github.com>
2022-11-01 20:25:13 -0400

    Update 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,8 +2,8 @@
 #include <string.h>
 /**
  *create_file - create a file
- **filename: file
- **text_content: file content
+ *@filename: file
+ *@text_content: file content
  *Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
@@ -17,17 +17,14 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	
 	if (fd == -1 && rewrt == -1)
 	{
 		return (-1);
 	}
-
 	if (text_content == NULL)
 	{
 		text_content = "";
 	}
-
 	if (fd == 0000)
 	{
 		return (-1);
@@ -36,4 +33,3 @@ int create_file(const char *filename, char *text_content)
 	close(fd);
 	return (1);
 }
-
