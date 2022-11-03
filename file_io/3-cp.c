f9ba2f5ab0908c08f6965a87842103dee738c09f
JimmyHernandez <you@example.com>
2022-11-03 18:48:45 +0000

    second commit

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,38 +9,39 @@ int main(int argc, char *argv[])
 {
 	int fd, fd2, input = 1, output = 1;
 	char buffer[1024];
-
+	/*validate amout of arguments*/
 	if (argc != 3)
 	{
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	} /*open file*/
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	}
+	/*open file 1*/
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	}
+	/*create file 2*/
 	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd2 == -1)
 	{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	} /*read file*/
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
 	while (input)
-	{
+	{	/*read from file 1*/
 		input = read(fd, buffer, 1024);
 	if (input == -1)
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	/*print file*/
+
+		/*print to file 2*/
 		if (input > 0)
-		{
 		output = write(fd2, buffer, input);
-	if (output == -1)
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-		}
-	} /*close file descriptors*/
+		if (output == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+	/*close file descriptors*/
 	output = close(fd);
 	if (output == -1)
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
-
 	output = close(fd2);
 	if (output == -1)
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
