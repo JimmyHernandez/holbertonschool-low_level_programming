d9010c8cd284b5deff2f73e002d18f19dd6e8870
Jimmy Hernandez Rivera <105442329+JimmyHernandez@users.noreply.github.com>
2022-11-03 15:29:34 -0400

    Update 2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -8,25 +8,27 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, rewtr, buffer = strlen(text_content);
+	int fd;
+	int numletters;
+	int WRITE;
 
 	if (!filename)
 		return (-1);
-	
+
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
-	{
 		return (-1);
-	}
 
-	rewtr = write(fd, text_content, buffer);
-
-	if (rewtr == -1)
+	if (text_content)
 	{
-		return (-1);
-	}
+		for (numletters = 0; text_content[numletters]; numletters++)
+			;
+		WRITE = write(fd, text_content, numletters);
 
-	close(fd);
+		if (WRITE == -1)
+			return (-1);
+	}
+	close (fd);
 	return (1);
 }
