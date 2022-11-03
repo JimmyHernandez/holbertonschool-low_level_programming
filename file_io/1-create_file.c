d8a6983c3213de3cdf0fb82c898f2a58c8e2193f
Jimmy Hernandez Rivera <105442329+JimmyHernandez@users.noreply.github.com>
2022-11-03 15:39:02 -0400

    Update 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,11 +10,6 @@ int create_file(const char *filename, char *text_content)
 {
 int fd, rewrt, buffer = strlen(text_content);
 	
-	if (!filename)
-	{
-		return (-1);
-	}
-
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	rewrt = write(fd, text_content, buffer);
 
