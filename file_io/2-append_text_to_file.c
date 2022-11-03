b16c47f4679b10958b9d4486c9160811a2046820
Jimmy Hernandez Rivera <105442329+JimmyHernandez@users.noreply.github.com>
2022-11-03 15:31:35 -0400

    Update 2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd, rewtr, buffer = strlen(text_content);
 
 	if (!filename)
-		return (-1)
+		return (-1);
 		
 	fd = open(filename, O_WRONLY | O_APPEND);
 
