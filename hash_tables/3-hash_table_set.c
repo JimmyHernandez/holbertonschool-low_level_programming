f7cfd3118e507530a35443979c091f309b081cf5
Jimmy Hernandez Rivera <105442329+JimmyHernandez@users.noreply.github.com>
2022-12-11 17:31:28 -0400

    Update 3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,53 +1,55 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_set - Adds a new element to the hash table
- * @ht: Pointer to hash table
- * @key: A non-empty character for key.
- * @value: Character for key's value.
- * Return: 1 (Success), 0 (Failure)
- **/
+ * add_n_hash - adds a node at the beginning of a hash at a given index
+ * @head: head of the hash linked list
+ * @key: key of the hash
+ * @value: value to store
+ * Return: head of the hash
+ */
+hash_node_t *add_n_hash(hash_node_t **head, const char *key,
+		const char *value)
+{
+	hash_node_t *tmp;
+
+	tmp = *head;
+	while (tmp != NULL)
+	{
+		if (strcmp(key, tmp->key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (*head);
+		}
+		tmp = tmp->next;
+	}
+	tmp = malloc(sizeof(hash_node_t));
+	if (tmp == NULL)
+		return (NULL);
+	tmp->key = strdup(key);
+	tmp->value = strdup(value);
+	tmp->next = *head;
+	*head = tmp;
+	return (*head);
+}
+
+/**
+ * hash_table_set - adds a hash (key, value) tp a given hash table
+ * @ht: pointer to the hash table
+ * @key: key of the hash
+ * @value: value to store
+ * Return: 1 Success, 0 Fail
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-        hash_node_t *NewEle;
-        char *ValCopy;
-        unsigned long int IDX, I;
+	unsigned long int k_index;
 
-        if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-        {
-                return (0);
-        }
-
-        ValCopy = strdup(value);
-        if (ValCopy == NULL)
-        {
-                return (0);
-        }
-
-        IDX = key_index((const unsigned char *)key, ht->size);
-        for (I = IDX; ht->array[I]; I++)
-        {
-                if (strcmp(ht->array[I]->key, key) == 0)
-                {
-                        free(ht->array[I]->value);
-                        ht->array[I]->value = ValCopy;
-                        return (1);
-                }
-        }
-
-        NewEle = malloc(sizeof(hash_node_t));
-        if (NewEle == NULL)
-        {
-                free(ValCopy);
-                return (0);
-        }
-        NewEle->key = strdup(key);
-        if (NewEle->key == NULL)
-        {
-                free(NewEle);
-                return (0);
-        }
-        NewEle->value = ValCopy;
-        NewEle->next = ht->array[IDX];
-        ht->array[IDX] = NewEle;
-        return (1);
+	if (ht == NULL)
+		return (0);
+	if (key == NULL || *key == '\0')
+		return (0);
+	k_index = key_index((unsigned char *)key, ht->size);
+	if (add_n_hash(&(ht->array[k_index]), key, value) == NULL)
+		return (0);
+	return (1);
 }
