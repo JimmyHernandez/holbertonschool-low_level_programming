79fd9f3fc1c0373f69a2430e3cfa85d5dca5ef1d
Jimmy Hernandez Rivera <105442329+JimmyHernandez@users.noreply.github.com>
2022-12-11 17:09:57 -0400

    Update 0-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	ht->size = size;
-	ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
+	ht->array = malloc((size_t)ht->size, sizeof(hash_node_t *));
 	if (ht->array == NULL)
 	{
 		return (NULL);
