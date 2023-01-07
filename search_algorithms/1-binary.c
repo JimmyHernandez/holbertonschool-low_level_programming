e820ba77c3744fbf75baf862387a37912d8bc9e1
JimmyHernandez <you@example.com>
2023-01-07 18:20:41 +0000

    first commit

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,4 +1,4 @@
-#included "search_algos.h"
+#include "search_algos.h"
 /**
 * binary_search - search a number with binary search.
 *@array: array
@@ -8,5 +8,28 @@
 */
 int binary_search(int *array, size_t size, int value)
 {
-    return 0;
+	size_t left = 0;
+	size_t idx = 0;
+	size_t right = size - 1;
+	size_t middle = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (idx = left; idx <= (right - 1); idx++)
+			printf("%d, ", array[idx]);
+		printf("%d\n", array[right]);
+		middle = ((left + right) / 2);
+		if (array[middle] < value)
+			left = middle + 1;
+		else if (array[middle] > value)
+			right = middle + 1;
+		else
+			return (middle);
+	}
+	return (-1);
 }
+
