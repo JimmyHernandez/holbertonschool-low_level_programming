c999b9918d2df1c38c7cddd92592d88b6bf83654
JimmyHernandez <you@example.com>
2023-01-07 17:38:21 +0000

    first commit

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,12 @@
+#included "search_algos.h"
+/**
+* binary_search - search a number with binary search.
+*@array: array
+*@size: size
+*@value: value
+*Return:  the selecte number.
+*/
+int binary_search(int *array, size_t size, int value)
+{
+    return 0;
+}
