b56741f69eaccaeec7cb55b240621977d71c7906
JimmyHernandez <you@example.com>
2023-01-07 17:52:30 +0000

    fix betty

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,6 +1,6 @@
 #include "search_algos.h"
 /**
-* linear_search : look file with linear search
+* linear_search - look file with linear search
 *@array: array
 *@size: size
 *@value: value
@@ -9,15 +9,17 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    size_t idx;
+size_t idx;
 
-    if(array == NULL)
-        return (-1);
-    for(idx = 0; idx < size; idx++)
-    {
-        printf("Value cheked array[%ld] = [%d]\n", idx, array[idx]);
-        if(array[idx] == value)
-            return (idx);
-    }
-    return -1;
+if (array == NULL)
+return (-1);
+
+for (idx = 0; idx < size; idx++)
+{
+printf("Value checked array[%ld] = [%d]\n", idx, array[idx]);
+if (array[idx] == value)
+return (idx);
 }
+return (-1);
+}
+
